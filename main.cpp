0998a7c7fd9fabe4d71d7a2f1b8683c8423bb291
stefiitg <tiganilastefania23@gmail.com>
2025-11-02 16:43:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,140 @@
 #include <iostream>
-#include <array>
+#include <string>
+using namespace std;
+
+
+class Character {
+protected:
+    string name;
+    int x, y;
+    int lives;
+
+public:
+    Character(string n = "Anonim", int x0 = 0, int y0 = 0, int v = 3)
+         {
+        name=n;
+        x=x0; y=y0;
+        lives=v;
+
+        cout << "Constructor Character apelat pentru " << name << endl;
+    }
+
+    Character(const Character& other)
+        {
+        name=other.name;
+        x=other.x; y=other.y;
+        lives=other.lives;
+        cout << "Copiere Character pentru " << name << endl;
+    } ///constructoru de copiere
+
+    virtual ~Character() {
+        cout << "Destructor Character apelat pentru " << name << endl;
+    }
+
+    virtual void moveLeft() { x--; }
+    virtual void moveRight() { x++; }
+    virtual void jump() { y += 2; }
+    virtual void takeDamage() {
+        if (lives > 0) lives--;
+    }
+
+    int getX() const { return x; }
+    int getY() const { return y; }
+    int getLives() const { return lives; }
+    string getName() const { return name; }
+
+    friend ostream& operator<<(ostream& os, const Character& c) {
+        os << c.name << " este la (" << c.x << ", " << c.y
+           << ") si are " << c.lives << " vieti.";
+        return os;
+    }
+};
+
+
+class Fireboy : public Character {
+public:
+    Fireboy(int x0 = 0, int y0 = 0) : Character("Fireboy", x0, y0) {} //luam la constrctor doar caracteristiciile lui character
+
+    void touchFire() {
+        cout << "Fireboy este imun la foc." << endl;
+    }
+
+    void touchWater() {
+        cout << "Fireboy a atins apa si a pierdut o viata!" << endl;
+        takeDamage();
+    }
+};
+
+
+class Watergirl : public Character {
+public:
+    Watergirl(int x0 = 0, int y0 = 0) : Character("Watergirl", x0, y0) {}
+
+    void touchWater() {
+        cout << "Watergirl este imuna la apa." << endl;
+    }
+
+    void touchFire() {
+        cout << "Watergirl a atins focul si pierdut o viata!" << endl;
+        takeDamage();
+    }
+};
+
+
+class Level {
+private:
+    Fireboy fireboy;
+    Watergirl watergirl;
+    int exitX, exitY;
+
+public:
+    Level(int fx = 0, int fy = 0, int wx = 5, int wy = 0, int ex = 10, int ey = 0) {
+        fireboy = Fireboy(fx, fy);
+        watergirl = Watergirl(wx, wy);
+        exitX = ex;
+        exitY = ey;
+
+        cout << "Nivel creat! Du-i pe fireboy si pe watergirl la iesire \n";
+    }
+
+    void playRound() {
+        cout << "\n--- Runda incepe ---\n";
+        fireboy.moveRight();
+        watergirl.moveRight();
+
+        fireboy.touchWater();
+        watergirl.touchFire();
+
+        cout << "\nPozitii curente:\n";
+        cout << fireboy << endl;
+        cout << watergirl << endl;
+
+        checkWin();
+    }
+
+    void checkWin() {
+        if (fireboy.getLives() <= 0 || watergirl.getLives() <= 0) {
+            cout << "Unul dintre personaje a murit. Game Over!!!!!!!\n";
+            return;
+        }
+
+        if (fireboy.getX() >= exitX && watergirl.getX() >= exitX) {
+            cout << " Ambii au ajuns la iesire! FELICITARI! ye\n";
+        } else {
+            cout << "Inca nu au ajuns la iesire.\n";
+        }
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
+
+    Level level1(0, 0, 3, 0, 10, 0);
+
+    for (int i = 1; i <= 4; i++) {
+        cout << "\nRunda " << i << ":\n";
+        level1.playRound();
     }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
+
     return 0;
 }
